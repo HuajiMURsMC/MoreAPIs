108bbd8c0942bd7c02eaa2d78ab4e0960e1119d0
HuajiMUR233 <13060847725@163.com>
2021-05-22 16:42:28 +0800

    0.0.3 > 0.0.4

--- MoreAPIs.py
+++ MoreAPIs.py
@@ -34,7 +34,7 @@ from mcdreforged.api.all import *
 
 
 _plugin_id = "more_apis"
-_plugin_version = "0.0.3"
+_plugin_version = "0.0.4"
 _mc_version = None
 _events = {
     "death_message": PluginEvent(_plugin_id + ".death_message"),
